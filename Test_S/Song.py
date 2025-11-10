b85efa2c4dc2e257c2d94e7c70b9324b422e7f65
송현승 <songhyeolseung@gmail.com>
2025-11-10 17:40:21 +0900

    Refactor typewriter effect to support per-character delays
    Changed the lyrics data structure to separate text and delay maps, allowing for per-character delays in the typewriter effect. Updated the Utils_S class methods to handle lists of delays for each character, improving the granularity of the typewriter animation.

--- Test_S/Song.py
+++ Test_S/Song.py
@@ -8,37 +8,48 @@ from typing import List, Union, Any, Dict, Tuple
 
 def 사랑하게_될_거야():
     lyrics = [
-        ("아", 2),
-        ("뭐가 그리 샘이 났길래", 0.05),
-        ("그토록 휘몰아쳤던가", 0.05),
-        ("그럼에도 불구하고", 0.05),
-        ("나는 너를 용서하고", 0.05),
-        ("사랑하게 될 거야", 0.05)
+        "아",
+        "뭐가 그리 샘이 났길래",
+        "그토록 휘몰아쳤던가",
+        "그럼에도 불구하고",
+        "나는 너를 용서하고",
+        "사랑하게 될 거야"
     ]
 
-    Utils_S().typewriter_lines(lyrics, line_delay=0.3)
+    delay_map = [
+        [1.5],  # 아
+        [0.32, 0.32, 0.05, 0.3, 0.4, 0.05, 0.35, 0.35, 0.05, 0.63, 0.68, 0.4],  # 뭐가 그리 샘이 났길래
+        [0.1, 0.15, 0.1, 0.2, 0.15, 0.1, 0.2, 0.15, 0.1, 0.2],  # 그토록 휘몰아쳤던가
+        [0.1, 0.15, 0.1, 0.2, 0.15, 0.1, 0.2, 0.15, 0.1],  # 그럼에도 불구하고
+        [0.1, 0.15, 0.1, 0.2, 0.15, 0.1, 0.2, 0.15, 0.1, 0.2],  # 나는 너를 용서하고
+        [0.1, 0.15, 0.1, 0.2, 0.15, 0.1, 0.2, 0.15, 0.1]   # 사랑하게 될 거야
+    ]
+
+    delay_lyrics = (lyrics, delay_map)
+
+    Utils_S().typewriter_lines(delay_lyrics, line_delay=0.3)
     
 class Utils_S:
     def __init__(self):
         pass
 
-    def typewriter_effect(self, text: str, delay: float = 0.05) -> None:
+    def typewriter_effect(self, text: str, delay_map: float) -> None:
         """
         Simulate a typewriter effect by printing one character at a time with a delay.
         """
 
-        for char in text:
+        for idx, char in enumerate(text):
             sys.stdout.write(char)
             sys.stdout.flush()
-            time.sleep(delay)
+            time.sleep(delay_map[idx])
 
-    def typewriter_lines(self, texts: List[Tuple[str, float]], line_delay: float = 0.05) -> None:
+    def typewriter_lines(self, delay_lyrics: Tuple[List[str], List[float]], line_delay: float = 0.02) -> None:
         """
         Simulate a typewriter effect for a line of text, ending with a newline.
         """
 
-        for text, delay in texts:
-            self.typewriter_effect(text, delay=delay)
+        for text, delay_map in zip(*delay_lyrics):
+            self.typewriter_effect(text, delay_map)
             sys.stdout.write('\n')
             sys.stdout.flush()
             time.sleep(line_delay)
