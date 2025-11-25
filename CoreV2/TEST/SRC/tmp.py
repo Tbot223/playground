786a9f6eca6f2ba62390c557b70c7648ab7db38f
Tbot223 <104832561+Tbot223@users.noreply.github.com>
2025-11-26 03:07:58 +0900

    Delete tmp.py

--- CoreV2/TEST/SRC/tmp.py
+++
@@ -1,6 +0,0 @@
-from CoreV2 import AppCore
-
-a = AppCore.AppCore()
-
-result = a.find_keys_by_value({'a': 1, 'b': {'b1': 2, 'b2': 1}, 'c': 1, 'd': {'dd1' : 1, 'dd2': {'ddd1' : 1}}}, 1, "eq", True)
-print(result.data) 
